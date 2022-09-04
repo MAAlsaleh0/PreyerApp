3f6f674ab6e95841bd11f18caeeb5d99d9bf6f28
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-09-05 00:28:45 +0300

    Initial Commit

---
+++ PreyerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PreyerApp
+//
+//  Created by Mohammed Alsaleh on 09/02/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreyerApp/PreyerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreyerAppApp.swift
+//  PreyerApp
+//
+//  Created by Mohammed Alsaleh on 09/02/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct PreyerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
